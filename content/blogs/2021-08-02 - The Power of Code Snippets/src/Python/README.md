d38a8efda43a584ab83368e64e399fae891f3e1e
chrimaho <chrismahoney@hotmail.com>
2021-08-02 19:26:24 +1000

    Add The Power of Code Snippets

---
+++ content/blogs/2021-08-02 - The Power of Code Snippets/src/Python/README.md
@@ -0,0 +1,4 @@
+### Gists
+
+- [python-snippet-basic.json](https://gist.github.com/chrimaho/88eb8fc00a50c9715ae6c51eae3f616e)
+- [python-snippet-extended.json](https://gist.github.com/chrimaho/6930b1f578c4997f2f58dc6f4a11edd6)
---
+++ content/blogs/2021-08-02 - The Power of Code Snippets/src/Python/Snippets_Output.py
@@ -0,0 +1,54 @@
+#==============================================================================#
+#                                                                              #
+#    Title: Title                                                              #
+#    Purpose: Purpose                                                          #
+#    Notes: Notes                                                              #
+#    Author: chrimaho                                                          #
+#    Created: Created                                                          #
+#    References: References                                                    #
+#    Sources: Sources                                                          #
+#    Edited: Edited                                                            #
+#                                                                              #
+#==============================================================================#
+
+
+
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#    Heading1                                                               ####
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+
+#------------------------------------------------------------------------------#
+# Heading2                                                                  ####
+#------------------------------------------------------------------------------#
+
+# Heading3 ----
+def function_name(param_1_name:param_1_type=param_1_default, param_2_name:param_2_type=param_2_default):
+    '''
+    # function_name()
+    
+    Function summary.
+    
+    ### Args:
+        param_1_name(`param_1_type`, optional): param_1_description. Defaults to `param_1_default`.
+        param_2_name(`param_2_type`, optional): param_2_description. Defaults to `param_2_default`.
+    
+    ### Raises:
+        `AssertionError`: If any of the parameter types or attributes are incorrect.
+    
+    ### Returns:
+        `return_type`: Return description.
+    '''
+    
+    # Assertions
+    assert isinstance(param_1_name, param_1_type)
+    assert isinstance(param_2_name, param_2_type)
+    
+    # Do Work
+    #Here
+    
+    # Return
+    return None
